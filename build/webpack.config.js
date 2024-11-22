60603ac746cb1fd0129bad8a6e2fb4293b6ff2e1
wj29 <30924856+wj29@users.noreply.github.com>
2024-11-22 19:41:40 +0800

    fix

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,48 +6,49 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir);
 }
 
-module.exports = {
-  entry: resolve('src/main.js'), // 项目入口文件
-  output: {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue_test/' : '/',
-    path: resolve('dist'),
-    filename: 'js/[name].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/, // 处理 .vue 文件
-        use: 'vue-loader',
-      },
-      {
-        test: /\.js$/, // 处理 .js 文件
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/, // 处理 .css 文件
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      //vue$: 'vue/dist/vue.esm.js', // 指定 Vue 的完整版 vue3的不需要指定文件
-      '@': path.resolve(__dirname, 'src'), // 配置 @ 别名指向 src
+module.exports = env => {
+  return {
+    entry: resolve('src/main.js'), // 项目入口文件
+    output: {
+      path: resolve('dist'),
+      filename: env.production ? 'vue_test/js/[name].bundle.js' : 'js/[name].bundle.js',
     },
-    extensions: ['.js', '.vue', '.json'], // 自动解析的文件扩展名
-  },
-  plugins: [
-    new VueLoaderPlugin(), // 必须添加这个插件以支持 vue-loader
-    new HtmlWebpackPlugin({
-      template: resolve('src/index.html'), // 指定 HTML 模板
-      inject: 'body',  // 确保 JS 文件插入到 <body> 标签底部
-      publicPath: process.env.NODE_ENV === 'production' ? '/vue_test/' : '/',
-    }),
-  ],
-  devServer: {
-    static: resolve('dist'),
-    hot: true,
-  },
-  mode: 'development', // 设置开发模式
+    module: {
+      rules: [
+        {
+          test: /\.vue$/, // 处理 .vue 文件
+          use: 'vue-loader',
+        },
+        {
+          test: /\.js$/, // 处理 .js 文件
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/, // 处理 .css 文件
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        //vue$: 'vue/dist/vue.esm.js', // 指定 Vue 的完整版 vue3的不需要指定文件
+        '@': path.resolve(__dirname, 'src'), // 配置 @ 别名指向 src
+      },
+      extensions: ['.js', '.vue', '.json'], // 自动解析的文件扩展名
+    },
+    plugins: [
+      new VueLoaderPlugin(), // 必须添加这个插件以支持 vue-loader
+      new HtmlWebpackPlugin({
+        template: resolve('src/index.html'), // 指定 HTML 模板
+        inject: 'body',  // 确保 JS 文件插入到 <body> 标签底部
+        publicPath: process.env.NODE_ENV === 'production' ? '/vue_test/' : '/',
+      }),
+    ],
+    devServer: {
+      static: resolve('dist'),
+      hot: true,
+    },
+    mode: 'development', // 设置开发模式
+  }
 };
